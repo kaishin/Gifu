2cdfab104b86f7a0fd1ce03b54e69fdea31b96ab
Tony DiPasquale <tony@thoughtbot.com>
2014-12-18 19:43:45 -0500

    Fix speedup issues and out of bounds access

--- Source/AnimatedView.swift
+++ Source/AnimatedView.swift
@@ -4,28 +4,33 @@ import ImageIO
 public class AnimatedView: UIView {
   private var frames: [AnimatedFrame] = []
   private var currentFrameIndex = 0
+  private var displayLink: CADisplayLink?
+  private var timeSinceLastUpdate: NSTimeInterval = 0
 
   public var isAnimated: Bool {
     return frames.count > 1
   }
 
   public var isAnimating: Bool {
-    return !displayLink.paused
+    return !(displayLink?.paused ?? true)
   }
 
   public func setAnimatedFrames(frames: [AnimatedFrame]) {
     self.frames = frames
-    layer.contents = frames.first?.image?.CGImage
+    initialize()
     attachDisplayLink()
     pauseAnimation()
   }
 
-  private var displayLink = CADisplayLink()
-  private var timeSinceLastUpdate: NSTimeInterval = 0
+  private func initialize() {
+    layer.contents = frames.first?.image?.CGImage
+    currentFrameIndex = 0
+    timeSinceLastUpdate = 0
+  }
 
   func updateLayer() {
     let frame = frames[currentFrameIndex]
-    timeSinceLastUpdate += displayLink.duration
+    timeSinceLastUpdate += displayLink?.duration ?? 0
 
     if timeSinceLastUpdate >= frame.duration {
       timeSinceLastUpdate -= frame.duration
@@ -35,21 +40,22 @@ public class AnimatedView: UIView {
   }
 
   private func attachDisplayLink() {
+    if displayLink != .None { return }
     displayLink = CADisplayLink(target: self, selector: "updateLayer")
-    displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
+    displayLink?.addToRunLoop(NSRunLoop.mainRunLoop(), forMode: NSRunLoopCommonModes)
   }
 
   deinit {
-    displayLink.invalidate()
+    displayLink?.invalidate()
   }
 
   public func pauseAnimation() {
-    displayLink.paused = true
+    displayLink?.paused = true
   }
 
   public func resumeAnimation() {
     if isAnimated {
-      displayLink.paused = false
+      displayLink?.paused = false
     }
   }
 }
