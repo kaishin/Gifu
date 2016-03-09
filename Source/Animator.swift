8fc25e6fe5399a50b220cf57c2c8fa6d5bc4f5db
Storix <stuffokator@gmail.com>
2016-03-09 21:39:21 +0200

    Add functions for conversion between cache indices and GIF data indices

--- Source/Animator.swift
+++ Source/Animator.swift
@@ -86,6 +86,115 @@ class Animator {
     
     return AnimatedFrame(image: scaledImage, duration: frameDuration)
   }
+  
+  /// Updates the cached frames after moving to an arbitrary frame.
+  /// - parameter index: The index of the frame the timeline was moved to.
+  func prepareFramesAfterMovingToIndex(index: Int) {
+    if index < 0 || index >= frameCount {
+      return
+    }
+    
+    // Check whether all of the GIF frames are containted in the cache.
+    if animatedFrames.count == frameCount {
+      currentFrameIndex = index
+    } else {
+      rebuildCacheFromIndex(index)
+    }
+    
+    // Reset updating time.
+    timeSinceLastFrameChange = 0.0
+  }
+  
+  /// Rebuilds cache after frame moving in cases when all the frames cannot be loaded into the cache completely.
+  /// - parameter index: The index of the frame the timeline was moved to.
+  func rebuildCacheFromIndex(index: Int) {
+    let convertedIndices = convertCacheIndicesToGIFIndices()
+    
+    // Check whether cache rebuilding is needed.
+    if convertedIndices[currentFrameIndex] == index { return }
+    
+    // Calculate indices for preload.
+    var indicesForPreload = [Int](count: animatedFrames.count, repeatedValue: 0)
+    var baseIndex = index
+    for indexForPreload in (0..<indicesForPreload.count) {
+      indicesForPreload[indexForPreload] = baseIndex % frameCount
+      ++baseIndex
+    }
+    
+    // Reset all previous animation indices.
+    currentFrameIndex = 0
+    currentPreloadIndex = baseIndex % frameCount
+    
+    // Fill the cache with the new animated frames.
+    animatedFrames = indicesForPreload.reduce([]) { $0 + pure(prepareFrame($1)) }
+  }
+  
+  /// Maps from the currentFrameIndex to the index in the GIF's frames array.
+  /// - returns: Converted index.
+  func convertCurrentCacheIndexToGIFIndex() -> Int {
+    // Check whether the cache is empty.
+    if animatedFrames.count <= 0 { return -1 }
+    
+    // Check whether the cache doesn't require conversion.
+    if animatedFrames.count == frameCount { return currentFrameIndex }
+  
+    var convertedIndex: Int = currentPreloadIndex
+    for _ in (0..<animatedFrames.count) {
+      convertedIndex = (convertedIndex - 1) < 0 ? { convertedIndex = (frameCount - 1); return convertedIndex }() : --convertedIndex
+    }
+    
+    return convertedIndex
+  }
+  
+  /// Maps from cache array indices to GIF frames array indices.
+  /// - returns: An array with GIF frames array indices. Each index positon corresponds to the cache array index.
+  func convertCacheIndicesToGIFIndices() -> [Int] {
+    // Check whether the cache is empty.
+    if animatedFrames.count <= 0 { return [Int]() }
+    
+    // Check whether the cache doesn't require conversion.
+    if animatedFrames.count == frameCount { return (0..<frameCount).map{$0} }
+    
+    // Declare required vars.
+    var convertedIndices = [Int](count: animatedFrames.count, repeatedValue: 0)
+    var currentCachedFrameIndex = -1
+    var currentConvertedIndex = -1
+    
+    // Resets negative preload index so that it points to the max GIF frame.
+    func resetToMaxGIFIndex() -> Int {
+      currentConvertedIndex = (frameCount - 1)
+      return currentConvertedIndex
+    }
+
+    // Map negative indices to the max array index during decrement operations.
+    // Note: (currentFrameIndex - 1) index points to the correct position in GIF's frames array which is equal to (currentPreloadIndex - 1).
+    if (currentFrameIndex - 1) < 0 {
+      currentCachedFrameIndex = (animatedFrames.count - 1)
+    } else {
+      currentCachedFrameIndex = (currentFrameIndex - 1)
+    }
+    
+    if (currentPreloadIndex - 1) < 0 {
+      currentConvertedIndex = (frameCount - 1)
+    } else {
+      currentConvertedIndex = (currentPreloadIndex - 1)
+    }
+    
+    // Save already converted index.
+    convertedIndices[currentCachedFrameIndex] = currentConvertedIndex
+    
+    // Build left-hand side cache from the current cached frame index.
+    for lhsIndex in (0..<currentCachedFrameIndex).reverse() {
+      convertedIndices[lhsIndex] = (currentConvertedIndex - 1) < 0 ?  resetToMaxGIFIndex() : --currentConvertedIndex
+    }
+    
+    // Build right-hand side cache from the current cached frame index.
+    for rhsIndex in ((currentCachedFrameIndex + 1)..<self.animatedFrames.count).reverse() {
+      convertedIndices[rhsIndex] = (currentConvertedIndex - 1) < 0 ?  resetToMaxGIFIndex() : --currentConvertedIndex
+    }
+    
+    return convertedIndices
+  }
 
   /// Returns the frame at a particular index.
   ///
