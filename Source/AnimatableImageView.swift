639228163660eb531b44cc6673b82f889bddf20f
Storix <stuffokator@gmail.com>
2016-03-09 21:24:55 +0200

    Add protocol with optional methods to manage animation updates

--- Source/AnimatableImageView.swift
+++ Source/AnimatableImageView.swift
@@ -1,5 +1,30 @@
 import UIKit
 
+/// A protocol which defines methods that allow you to manage the animation progress and respond to the animation state changes.
+@objc
+public protocol AnimatableImageViewDelegate: class {
+  /// Tells the delegate that the animation has been started at the specified index.
+  /// - parameter animatableImageView: The AnimatableImageView object that is notifying the delegate about the animation starting.
+  /// - parameter index: The index of the GIF's frame the animation has beed started from. 
+  optional func animatableImageView(animatableImageView: AnimatableImageView, didStartAnimatingAtIndex index: Int)
+  
+  /// Tells the delegate that the animation has been stopped at the specified index.
+  /// - parameter animatableImageView: The AnimatableImageView object that is notifying the delegate about the animation stopping.
+  /// - parameter index: The index of the GIF's frame the animation has beed stopped at.
+  optional func animatableImageView(animatableImageView: AnimatableImageView, didStopAnimatingAtIndex index: Int)
+  
+  /// Tells the delegate that currently displayed frame has beed changed.
+  /// - parameter animatableImageView: The AnimatableImageView object that is notifying the delegate about the animation updates.
+  /// - parameter index: The index of the GIF's frame currently displayed frame was updated to.
+  optional func animatableImageView(animatableImageView: AnimatableImageView, didUpdateFrameToIndex index: Int)
+  
+  /// Tells the delegate that currently played animation cycle has reached the final frame.
+  /// - parameter animatableImageView: The AnimatableImageView object that is notifying the delegate about the finishing of the current animation iteration.
+  /// - parameter loopNumber: The number of the currently finished iteration of the animation playing.
+  /// - note: Moving to a specific animation frame resets the iterations count.
+  optional func animatableImageView(animatableImageView: AnimatableImageView, didReachEndOfCurrentLoop loopNumber: Int)
+}
+
 /// A subclass of `UIImageView` that can be animated using an image name string or raw data.
 public class AnimatableImageView: UIImageView {
   /// An `Animator` instance that holds the frames of a specific image in memory.
@@ -7,7 +32,10 @@ public class AnimatableImageView: UIImageView {
   /// A display link that keeps calling the `updateFrame` method on every screen refresh.
   lazy var displayLink: CADisplayLink = CADisplayLink(target: self, selector: Selector("updateFrame"))
 
-  /// The size of the frame cache.
+  /// The object that acts as the delegate of the AnimatableImageView.
+  public weak var delegate: AnimatableImageViewDelegate?
+  
+  /// The size of the frame cache. Default is 50.
   public var framePreloadCount = 50
 
   /// A computed property that returns whether the image view is animating.
