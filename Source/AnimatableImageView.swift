a2cd471e52ecd6bd662c5464a99b026c50b6da83
Shantanu Desai <gokuman76@gmail.com>
2016-03-31 00:36:55 +0530

    Update to iOS 9.3 #selector() syntax.

--- Source/AnimatableImageView.swift
+++ Source/AnimatableImageView.swift
@@ -5,7 +5,7 @@ public class AnimatableImageView: UIImageView {
   /// An `Animator` instance that holds the frames of a specific image in memory.
   var animator: Animator?
   /// A display link that keeps calling the `updateFrame` method on every screen refresh.
-  lazy var displayLink: CADisplayLink = CADisplayLink(target: self, selector: Selector("updateFrame"))
+  lazy var displayLink: CADisplayLink = CADisplayLink(target: self, selector: #selector(updateFrame))
 
   /// The size of the frame cache.
   public var framePreloadCount = 50
