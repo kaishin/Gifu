d9c474d553e43f50838bfd5fd09053600c06214c
Reda Lemeden <git@kaishin.haz.email>
2016-09-25 11:18:01 +0200

    Make pre-scaling off by default

--- Source/AnimatableImageView.swift
+++ Source/AnimatableImageView.swift
@@ -33,8 +33,8 @@ public class AnimatableImageView: UIImageView {
   /// The size of the frame cache.
   public var framePreloadCount = 50
 
-  /// Specifies whether the GIF frames should be pre-scaled to save memory. Default is **true**.
-  public var needsPrescaling = true
+  /// Specifies whether the GIF frames should be pre-scaled to save memory. Default is **false**.
+  public var needsPrescaling = false
 
   /// A computed property that returns whether the image view is animating.
   public var isAnimatingGIF: Bool {
