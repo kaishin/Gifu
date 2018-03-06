2e196c221e0fc065e4e9e4b35fe462cffbc2460e
Reda Lemeden <git@kaishin.haz.email>
2018-03-06 07:24:24 +0100

    Unify framework  names across platforms
    - Remove header comments
    - Rename tvOS target to use dash
    - Closes #122

---
+++ Gifu-tvOS/Gifu-tvOS.h
@@ -0,0 +1,4 @@
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double Gifu-tvOSVersionNumber;
+FOUNDATION_EXPORT const unsigned char Gifu-tvOSVersionString[];
--- Gifu-tvOSTests/Gifu-tvOSTests.swift
+++ Gifu-tvOSTests/Gifu-tvOSTests.swift
@@ -1,14 +1,6 @@
-//
-//  Gifu_tvOSTests.swift
-//  Gifu_tvOSTests
-//
-//  Created by Gruppioni Michele on 17/02/18.
-//  Copyright © 2018 Kaishin & Co. All rights reserved.
-//
-
 import XCTest
 import ImageIO
-@testable import Gifu_tvOS
+@testable import Gifu
 
 private let imageData = testImageDataNamed("mugen.gif")
 private let staticImage = UIImage(data: imageData)!
@@ -28,69 +20,69 @@ class GifuTests: XCTestCase {
     var animator: Animator!
     var originalFrameCount: Int!
     let delegate = DummyAnimatable()
-    
+
     override func setUp() {
         super.setUp()
         animator = Animator(withDelegate: delegate)
         animator.prepareForAnimation(withGIFData: imageData, size: staticImage.size, contentMode: .scaleToFill)
         originalFrameCount = 44
     }
-    
+
     func testIsAnimatable() {
         XCTAssertNotNil(animator.frameStore)
         guard let store = animator.frameStore else { return }
         XCTAssertTrue(store.isAnimatable)
     }
-    
+
     func testCurrentFrame() {
         XCTAssertNotNil(animator.frameStore)
         guard let store = animator.frameStore else { return }
         XCTAssertEqual(store.currentFrameIndex, 0)
     }
-    
+
     func testFramePreload() {
         XCTAssertNotNil(animator.frameStore)
         guard let store = animator.frameStore else { return }
-        
+
         let expectation = self.expectation(description: "frameDuration")
-        
+
         store.prepareFrames {
             let animatedFrameCount = store.animatedFrames.count
             XCTAssertEqual(animatedFrameCount, self.originalFrameCount)
             XCTAssertNotNil(store.frame(at: preloadFrameCount - 1))
             XCTAssertNil(store.frame(at: preloadFrameCount + 1)?.images)
             XCTAssertEqual(store.currentFrameIndex, 0)
-            
+
             store.shouldChangeFrame(with: 1.0) { hasNewFrame in
                 XCTAssertTrue(hasNewFrame)
                 XCTAssertEqual(store.currentFrameIndex, 1)
                 expectation.fulfill()
             }
         }
-        
+
         waitForExpectations(timeout: 1.0) { error in
             if let error = error {
                 print("Error: \(error.localizedDescription)")
             }
         }
     }
-    
+
     func testFrameInfo() {
         XCTAssertNotNil(animator.frameStore)
         guard let store = animator.frameStore else { return }
-        
+
         let expectation = self.expectation(description: "testFrameInfoIsAccurate")
-        
+
         store.prepareFrames {
             let frameDuration = store.frame(at: 5)?.duration ?? 0
             XCTAssertTrue((frameDuration - 0.05) < 0.00001)
-            
+
             let imageSize = store.frame(at: 5)?.size ?? CGSize.zero
             XCTAssertEqual(imageSize, staticImage.size)
-            
+
             expectation.fulfill()
         }
-        
+
         waitForExpectations(timeout: 1.0) { error in
             if let error = error {
                 print("Error: \(error.localizedDescription)")
