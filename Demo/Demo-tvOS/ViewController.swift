9306c9715a925bdef05935265b11d333aabb2d92
Reda Lemeden <git@redalemeden.com>
2019-11-16 22:42:43 +0100

    Cleanup demo project

---
+++ Demo/Demo-tvOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gifu-tvOSDemo
+//
+//  Created by Reda Lemeden on 2019-11-16.
+//  Copyright © 2019 Kaishin & Co. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
