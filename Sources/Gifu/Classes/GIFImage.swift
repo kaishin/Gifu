c522f5819174acc0868b53febd8c31e012bbaf07
kean <grebenyuk.alexander@gmail.com>
2023-02-05 22:19:38 -0500

    Fix GIFImage resize behavior

--- Sources/Gifu/Classes/GIFImage.swift
+++ Sources/Gifu/Classes/GIFImage.swift
@@ -3,15 +3,10 @@ import SwiftUI
 
 /// An image view for displaying animated images.
 @available(iOS 13, tvOS 13, *)
-public struct GIFImage: UIViewRepresentable {
-    private enum Source {
-        case data(Data)
-        case url(URL)
-        case imageName(String)
-    }
-
-    private let source: Source
+public struct GIFImage: View {
+    private let source: GIFSource
     private var loopCount = 0
+    private var isResizable = false
 
     /// Initializes the view with the given GIF image data.
     public init(data: Data) {
@@ -35,19 +30,52 @@ public struct GIFImage: UIViewRepresentable {
         return copy
     }
 
-    public func makeUIView(context: Context) -> GIFImageView {
-        GIFImageView(frame: .zero)
+    /// Sets an image to fit its space.
+    public func resizable() -> GIFImage {
+        var copy = self
+        copy.isResizable = true
+        return copy
     }
 
-    public func updateUIView(_ view: GIFImageView, context: Context) {
+    public var body: some View {
+        _GIFImage(source: source, loopCount: loopCount, isResizable: isResizable)
+    }
+}
+
+@available(iOS 13, tvOS 13, *)
+private struct _GIFImage: UIViewRepresentable {
+    let source: GIFSource
+    let loopCount: Int
+    let isResizable: Bool
+
+    func makeUIView(context: Context) -> GIFImageView {
+        let imageView = GIFImageView()
+        if isResizable {
+            imageView.setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
+            imageView.setContentCompressionResistancePriority(.defaultLow, for: .vertical)
+        }
+        return imageView
+    }
+
+    func updateUIView(_ imageView: GIFImageView, context: Context) {
         switch source {
         case .data(let data):
-            view.animate(withGIFData: data, loopCount: loopCount)
+            imageView.animate(withGIFData: data, loopCount: loopCount)
         case .url(let url):
-            view.animate(withGIFURL: url, loopCount: loopCount)
+            imageView.animate(withGIFURL: url, loopCount: loopCount)
         case .imageName(let imageName):
-            view.animate(withGIFNamed: imageName, loopCount: loopCount)
+            imageView.animate(withGIFNamed: imageName, loopCount: loopCount)
         }
     }
+
+    static func dismantleUIView(_ imageView: GIFImageView, coordinator: ()) {
+        imageView.prepareForReuse()
+    }
+}
+
+private enum GIFSource {
+    case data(Data)
+    case url(URL)
+    case imageName(String)
 }
 #endif
